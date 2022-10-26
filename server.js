0265ba13f4e54d5576dcd7983492633ba63077eb
Jasmine <wrkerr9@gmail.com>
2022-10-26 13:32:25 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = 80;
+
+app.use(express.static('css'))
+app.use(express.static('./'))
+app.listen(PORT, () => {
+    console.log("Listening on port %s", PORT)
+})
